649888826011edf72b7e76e5e38efa4d4f2de3da
taissalomao <tais.salomao@fatec.sp.gov.br>
2023-06-07 00:24:44 -0300

    Alterações de estilo na home e agora o total tanto de receita quanto de despesa é retornado na tela.

--- src/views/home.js
+++ src/views/home.js
@@ -1,199 +1,217 @@
-/* eslint-disable prettier/prettier */
-/* eslint-disable react/self-closing-comp */
-/* eslint-disable prettier/prettier */
-/* eslint-disable prettier/prettier */
-import React from 'react';
-import {View, Text, StyleSheet, Alert, BackHandler} from 'react-native';
-import {Button, NativeBaseProvider} from 'native-base';
-import {useNavigation} from '@react-navigation/native';
+/* eslint-disable no-shadow *//* eslint-disable prettier/prettier */
+import React, { useEffect, useState } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { collection, query, getDocs, doc } from 'firebase/firestore';
+import { db } from '../config/firebaseConfig';
 import { useAuthentication } from '../utils/authenticator';
-import { useConfirmationExitEffect } from '../components/confirmExit';
-import { getAuth, signOut } from 'firebase/auth';
+import { Button } from 'native-base';
 
-
-function HomeScreen(/* { username } */) {
-  useConfirmationExitEffect();
+export default function HomeScreen() {
+  const [totalDespesas, setTotalDespesas] = useState(0);
+  const [totalReceitas, setTotalReceitas] = useState(0);
+  const [userName, setUserName] = useState('');
   const navigation = useNavigation();
+  const { user } = useAuthentication();
 
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        if (!user) {
+          return;
+        }
+
+        const userDocRef = doc(db, 'user', user.uid);
+        const userDocSnapshot = await userDocRef.get();
+        if (userDocSnapshot.exists()) {
+          const userData = userDocSnapshot.data();
+          if (userData && userData.nome) {
+            setUserName(userData.nome);
+          }
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchUserData();
+  }, [user]);
+
+  useEffect(() => {
+    const fetchTotalDespesas = async () => {
+      try {
+        if (!user) {
+          return;
+        }
+
+        const userDocRef = doc(db, 'user', user.uid);
+        const expensesRef = collection(userDocRef, 'despesas');
+        const q = query(expensesRef);
+        const querySnapshot = await getDocs(q);
+        let total = 0;
+        querySnapshot.forEach((doc) => {
+          const expense = doc.data();
+          total += expense.valor;
+        });
+        setTotalDespesas(total);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchTotalDespesas();
+  }, [user]);
+
+  useEffect(() => {
+    const fetchTotalReceitas = async () => {
+      try {
+        if (!user) {
+          return;
+        }
+
+        const userDocRef = doc(db, 'user', user.uid);
+        const receitasRef = collection(userDocRef, 'receitas');
+        const q = query(receitasRef);
+        const querySnapshot = await getDocs(q);
+        let total = 0;
+        querySnapshot.forEach((doc) => {
+          const receita = doc.data();
+          total += receita.valor;
+        });
+        setTotalReceitas(total);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchTotalReceitas();
+  }, [user]);
+
+  const navigateToProfile = () => {
+    navigation.navigate('Perfil');
+  };
 
   return (
-    <NativeBaseProvider>
-      <View style={styles.wrapper}>
-        {/*          <Text style={styles.buttonTexto}>Bem vindo{username}</Text> */}
-        <View style={styles.container}>
-          <Text style={styles.username}>Bem vindo{/* {username} */}</Text>
-        </View>
-        <View style={styles.overlay}>
-          <Text style={styles.texto}>Saldo</Text>
-          <Text style={styles.texto}>R$ 0,00</Text>
-          <Button
-            style={styles.buttonMinhasReceitas}
-            onPress={() => {
-              navigation.navigate('CadastroReceita');
-            }}>
-            <Text style={styles.buttonTexto}>Adicionar Receita</Text>
-          </Button>
-        </View>
-        <View style={styles.content}>
-          <Button
-            style={styles.buttonReceita}
-            onPress={() => {
-              navigation.navigate('Receitas');
-            }}>
-            <Text style={styles.buttonTexto}>Minhas Receitas</Text>
-          </Button>
-        </View>
-        <View style={styles.overlay2}>
-        <Text style={styles.texto}>Despesas</Text>
-          <Text style={styles.texto}>R$ 0,00</Text>
-        <Button
-            style={styles.buttonDespesa}
-            onPress={() => {
-              navigation.navigate('CadastroDespesa');
-            }}>
-            <Text style={styles.buttonTexto}>Adicionar Despesa</Text>
-          </Button>
-        </View>
-        <View style={styles.content2}>
-        <Button
-            style={styles.buttonReceita}
-            onPress={() => {
-              navigation.navigate('Despesas');
-            }}>
-            <Text style={styles.buttonTexto}>Minhas Despesas</Text>
-          </Button>
-        </View>
-        <View>
-          <Button
-            style={styles.buttonProfile}
-            onPress={() => {
-              navigation.navigate('Perfil');
-            }}>
-            <Text style={styles.buttonTexto}>Meu perfil</Text>
-          </Button>
-        </View>
-        <View>
-          <Button
-            style={styles.buttonProfile}
-            onPress={() => {
-              navigation.navigate('Charts');
-            }}>
-            <Text style={styles.buttonTexto}>Meus relatórios</Text>
-          </Button>
-        </View>
+    <View style={styles.container}>
+      <View style={styles.topSection}>
+        <Text style={styles.welcome}>Bem-vindo, {userName}!</Text>
+        <Button style={styles.avatarButton} onPress={navigateToProfile} />
       </View>
-    </NativeBaseProvider>
+
+      <View style={styles.sectionContainer}>
+        <Text style={styles.sectionTitle}>Receitas R$ {totalReceitas.toFixed(2)}</Text>
+        <Button
+          style={styles.button}
+          onPress={() => navigation.navigate('Receitas')}
+        >
+          <Text style={styles.buttonText}>Minhas Receitas</Text>
+        </Button>
+        <Button
+          style={styles.button}
+          onPress={() => navigation.navigate('CadastroReceita')}
+        >
+          <Text style={styles.buttonText}>Cadastrar Receita</Text>
+        </Button>
+      </View>
+
+      <View style={styles.sectionContainer}>
+        <Text style={styles.sectionTitle}>Despesas R$ {totalDespesas.toFixed(2)}</Text>
+        <Button
+          style={styles.button}
+          onPress={() => navigation.navigate('Despesas')}
+        >
+          <Text style={styles.buttonText}>Minhas Despesas</Text>
+        </Button>
+        <Button
+          style={styles.button}
+          onPress={() => navigation.navigate('CadastroDespesa')}
+        >
+          <Text style={styles.buttonText}>Cadastrar Despesa</Text>
+        </Button>
+      </View>
+
+      <Button
+        style={styles.secondaryButton}
+        onPress={() => navigation.navigate('Charts')}
+      >
+        <Text style={styles.buttonText}>Meus Relatórios</Text>
+      </Button>
+    </View>
   );
 }
 
-export default HomeScreen;
 
 const styles = StyleSheet.create({
-  wrapper: {
-    position: 'relative',
-    flex: 1,
-    backgroundColor: '#e8e9eb', //fundo aqui
-  },
   container: {
-    paddingTop: 80,
-    paddingHorizontal: 20,
-    backgroundColor: '#3005f2',
+    flex: 1,
+    backgroundColor: '#e8e9eb',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  topSection: {
+    flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#1348cf',
+    width: '100%',
+    paddingTop: 30,
+    paddingBottom: 20,
   },
-  username: {
+  welcome: {
     color: '#fff',
     fontSize: 24,
     fontWeight: 'bold',
+    marginRight: 10,
+  },
+  sectionContainer: {
+    backgroundColor: '#fff',
+    borderRadius: 16,
+    paddingHorizontal: 16,
+    paddingVertical: 24,
+    marginTop: 20,
+    elevation: 2,
+    width: '90%',
+  },
+  sectionTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 16,
+    color: '#1348cf',
+  },
+  button: {
+    backgroundColor: '#79d6f7',
+    borderRadius: 16,
+    height: 52,
     marginTop: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
-  overlay: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-    backgroundColor: '#f8f8f8',
-    borderRadius: 16,
-    margin: 24,
-    height: 200,
-    marginTop: 80,
-    boxShadow: '0px 8px 8px rgba(0, 0, 0, 0.1)',
-    border: '1px solid #f8f8f8',
-  },
-  content: {
-    flex: 2,
-    margin: 32,
-  },
-  content2: {
-    margin: 32,
-    flex: 2,
-  },
-  overlay2: {
-    position: 'absolute',
-    flex: 1,
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-    backgroundColor: '#f8f8f8',
-    borderRadius: 16,
-    margin: 24,
-    height: 200,
-    marginTop: 300,
-    boxShadow: '0px 8px 8px rgba(0, 0, 0, 0.1)',
-    border: '1px solid #f8f8f8',
-  },
-  buttonReceita: {
-    backgroundColor: '#79d6f7',
-    marginTop: 10,
-    marginLeft: 10,
-    marginRight: 10,
-    marginBottom: 50,
-    borderRadius: 16,
-    height: 52,
-  },
-  buttonMinhasReceitas: {
-    backgroundColor: '#79d6f7',
-    marginTop: 100,
-    marginLeft: 10,
-    marginRight: 10,
-    borderRadius: 16,
-    padding: 100,
-    height: 52,
-  },
-
-  buttonDespesa: {
-    backgroundColor: '#79d6f7',
-    marginTop: 50,
-    marginLeft: 10,
-    marginRight: 10,
-    marginBottom: 50,
-    borderRadius: 16,
-    height: 52,
-  },
-  buttonTexto: {
+  buttonText: {
     color: '#1348cf',
     fontWeight: 'bold',
+    fontSize: 16,
   },
-  texto: {
-    color: '#1348cf',
-    fontWeight: 'bold',
-    fontSize: 10,
-    marginLeft: 10,
-    marginRight: 10,
-  },
-  buttonProfile: {
+  secondaryButton: {
     backgroundColor: '#79d6f7',
-    marginTop: 10,
-    marginLeft: 40,
-    marginRight: 40,
-    marginBottom: 50,
     borderRadius: 16,
+    height: 52,
+    paddingHorizontal: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 20,
   },
-  welcomeTexto: {
+  avatarButton: {
     backgroundColor: '#79d6f7',
-    color: '#1348cf',
+    borderRadius: 50,
+    width: 40,
+    height: 40,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+  },
+  avatarText: {
+    color: '#fff',
+    fontSize: 20,
     fontWeight: 'bold',
   },
 });
