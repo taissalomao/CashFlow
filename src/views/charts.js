a15f83dd1c4cb1faea45b579d429d506dcac36b4
MatheusSakuragui <matheus.lemesakuragui@gmail.com>
2023-06-11 15:27:00 -0300

    Ajustado charts

--- src/views/charts.js
+++ src/views/charts.js
@@ -10,13 +10,15 @@ import { useAuthentication } from "../utils/authenticator";
 
 export default function ChartScreen() {
   const [expenseData, setExpenseData] = useState(null);
+  const [incomeData, setIncomeData] = useState(null);
   const { user } = useAuthentication();
 
-  const handleDataPointClick = (data, index) => {
-    console.log(index)
-    console.log(data.dataset.data[0])
+  const handleDataPointClick = (data, index, isExpense) => {
+    console.log(index);
+    console.log(data.dataset.data[index]);
     const value = data.dataset.data[index];
-    Alert.alert("Valor", `Valor: ${value}`);
+    const dataType = isExpense ? "Despesa" : "Receita";
+    Alert.alert("Valor", `${dataType}: ${value}`);
   };
 
   useEffect(() => {
@@ -37,8 +39,41 @@ export default function ChartScreen() {
       }
     };
 
+    const fetchIncomes = async () => {
+      try {
+        const userDocRef = doc(db, "user", user?.uid);
+        const incomesRef = collection(userDocRef, "Receitas");
+        const q = query(incomesRef);
+        const querySnapshot = await getDocs(q);
+        const incomeList = [];
+        querySnapshot.forEach((doc) => {
+          const income = { id: doc.id, ...doc.data() };
+          incomeList.push(income);
+        });
+        setIncomeData(incomeList);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
     fetchExpenses();
+    fetchIncomes();
   }, [user, user?.uid]);
+
+  const calculateProfitData = () => {
+    if (incomeData && expenseData) {
+      const profitData = incomeData.map((income, index) => {
+        const expense = expenseData[index];
+        const profit = income.valor - expense.valor;
+        return {
+          label: income.label,
+          valor: profit,
+        };
+      });
+      return profitData;
+    }
+    return [];
+  };
 
   return (
     <>
@@ -48,49 +83,128 @@ export default function ChartScreen() {
           flex: 1,
           paddingTop: 5,
         }}
-        contentContainerStyle={{ flexGrow: 1, justifyContent: "center" }}
-        horizontal
       >
-        <Text>Despesas</Text>
-        {expenseData && (
-          <LineChart
-            data={{
-              labels: expenseData.map((expense) => expense.label),
-              datasets: [
-                {
-                  data: expenseData.map((expense) => expense.valor),
-                },
-              ],
-            }}
-            width={Dimensions.get("window").width * 2} // Multiplica por 2 para que o gráfico seja mais largo que a tela
-            height={300}
-            yAxisLabel="$"
-            yAxisSuffix="k"
-            yAxisInterval={1} // optional, defaults to 1
-            chartConfig={{
-              backgroundColor: "#e26a00",
-              backgroundGradientFrom: "#fb8c00",
-              backgroundGradientTo: "#ffa726",
-              decimalPlaces: 2, // optional, defaults to 2dp
-              color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-              labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-              style: {
-                borderRadius: 16,
-              },
-              propsForDots: {
-                r: "6",
-                strokeWidth: "2",
-                stroke: "#ffa726",
-              },
-            }}
-            bezier
-            style={{
-              marginVertical: 8,
-              borderRadius: 16,
-            }}
-            onDataPointClick={({ index }) => handleDataPointClick(expenseData, index)}
-          />
-        )}
+        <ScrollView
+          horizontal
+          showsHorizontalScrollIndicator={false}
+        >
+          <View style={{ flexDirection: 'column' }}>
+            <Text>Receitas</Text>
+            {incomeData && (
+              <LineChart
+                data={{
+                  labels: incomeData.map((income) => income.label),
+                  datasets: [
+                    {
+                      data: incomeData.map((income) => income.valor),
+                    },
+                  ],
+                }}
+                width={Dimensions.get("window").width * 2} // Multiplica por 2 para que o gráfico seja mais largo que a tela
+                height={300}
+                yAxisLabel="$"
+                yAxisSuffix="k"
+                yAxisInterval={1} // optional, defaults to 1
+                chartConfig={{
+                  backgroundColor: "#F6F6F6", // Nova cor de fundo do gráfico
+                  backgroundGradientFrom: "#F6F6F6", // Nova cor de gradiente de fundo do gráfico
+                  backgroundGradientTo: "#F6F6F6", // Nova cor de gradiente de fundo do gráfico
+                  decimalPlaces: 2,
+                  color: (opacity = 1) => `rgba(0, 122, 255, ${opacity})`, // Azul
+                  propsForDots: {
+                    r: "6",
+                    strokeWidth: "2",
+                    stroke: "#007AFF", // Azul brilhante
+                  },
+                }}
+                bezier
+                style={{
+                  marginVertical: 8,
+                  borderRadius: 16,
+                }}
+                onDataPointClick={({ index }) =>
+                  handleDataPointClick(incomeData, index, false)
+                }
+              />
+            )}
+            <Text>Despesas</Text>
+            {expenseData && (
+              <LineChart
+                data={{
+                  labels: expenseData.map((expense) => expense.label),
+                  datasets: [
+                    {
+                      data: expenseData.map((expense) => expense.valor),
+                    },
+                  ],
+                }}
+                width={Dimensions.get("window").width * 2} // Multiplica por 2 para que o gráfico seja mais largo que a tela
+                height={300}
+                yAxisLabel="$"
+                yAxisSuffix="k"
+                yAxisInterval={1} // optional, defaults to 1
+                chartConfig={{
+                  backgroundColor: "#F6F6F6", // Nova cor de fundo do gráfico
+                  backgroundGradientFrom: "#F6F6F6", // Nova cor de gradiente de fundo do gráfico
+                  backgroundGradientTo: "#F6F6F6", // Nova cor de gradiente de fundo do gráfico
+                  decimalPlaces: 2,
+                  color: (opacity = 1) => `rgba(255, 59, 48, ${opacity})`, // Vermelho
+                  propsForDots: {
+                    r: "6",
+                    strokeWidth: "2",
+                    stroke: "#FF3B30", // Vermelho brilhante
+                  },
+                }}
+                bezier
+                style={{
+                  marginVertical: 8,
+                  borderRadius: 16,
+                }}
+                onDataPointClick={({ index }) =>
+                  handleDataPointClick(expenseData, index, true)
+                }
+              />
+            )}
+            <Text>Lucro</Text>
+            {incomeData && expenseData && (
+              <LineChart
+                data={{
+                  labels: incomeData.map((income) => income.label),
+                  datasets: [
+                    {
+                      data: calculateProfitData().map((profit) => profit.valor),
+                    },
+                  ],
+                }}
+                width={Dimensions.get("window").width * 2} // Multiplica por 2 para que o gráfico seja mais largo que a tela
+                height={300}
+                yAxisLabel="$"
+                yAxisSuffix="k"
+                yAxisInterval={1} // optional, defaults to 1
+                chartConfig={{
+                  backgroundColor: "#F6F6F6", // Nova cor de fundo do gráfico
+                  backgroundGradientFrom: "#F6F6F6", // Nova cor de gradiente de fundo do gráfico
+                  backgroundGradientTo: "#F6F6F6", // Nova cor de gradiente de fundo do gráfico
+                  decimalPlaces: 2,
+                  color: (opacity = 1) => `rgba(40, 167, 69, ${opacity})`, // Verde
+                  propsForDots: {
+                    r: "6",
+                    strokeWidth: "2",
+                    stroke: "#28A745", // Verde brilhante
+                  },
+                }}
+                bezier
+                style={{
+                  marginVertical: 8,
+                  borderRadius: 16,
+                }}
+                onDataPointClick={({ index }) =>
+                  handleDataPointClick(calculateProfitData(), index, false)
+                }
+              />
+            )}
+          </View>
+        </ScrollView>
       </ScrollView>
     </>
   );
